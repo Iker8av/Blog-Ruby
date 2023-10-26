c6880d3662759975f80fe5ea3b7cd1505d091869
Santiago Vera <santiago.vera@oracle.com>
2023-10-25 21:36:46 -0600

    Finished all controller functions

--- app/controllers/kanto_controller.rb
+++ app/controllers/kanto_controller.rb
@@ -3,5 +3,8 @@ class KantoController < ApplicationController
     api_service = PokeApi.new
 
     @region_data = api_service.get_region "kanto"
+    @location_data = api_service.get_location "#{@region_data["locations"][0]["name"]}"
+
+    @test = api_service.get_location_pokemon_names "#{@region_data["locations"][0]["name"]}"
   end
 end
