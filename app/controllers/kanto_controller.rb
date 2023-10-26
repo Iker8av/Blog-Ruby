28248f344fae5ebdd2b7c2c3cf7894434b02e6ea
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-10-26 17:04:01 -0600

    .

--- app/controllers/kanto_controller.rb
+++ app/controllers/kanto_controller.rb
@@ -1,10 +1,28 @@
 class KantoController < ApplicationController
+  @name = "nul"
   def index
     api_service = PokeApi.new
 
     @region_data = api_service.get_region "kanto"
-    @location_data = api_service.get_location "#{@region_data["locations"][0]["name"]}"
 
     @test = api_service.get_location_pokemon_names "#{@region_data["locations"][0]["name"]}"
+    @name = 'place'
   end
+
+  def location
+    name = params[:name] # Get the n
+    api_service = PokeApi.new
+    @current_location = api_service.get_location(name) # Fetch the data based on the name
+    # @name = @current_location['name']
+    # render json: { location: @current_location } # Return the data as JSON
+    # @current_location = get_location(name)
+  end
+
+  def get_location
+    name = params[:name] # Get the n
+    api_service = PokeApi.new
+    @current_location = api_service.get_location(name) # Fetch the data based on the name
+    render json: { location: @current_location } # Return the data as JSON
+  end
+
 end
